94cac348d0c1ed23d627ac44cd62caa6bb0f50c8
nabeel392 <75073532+nabeel392@users.noreply.github.com>
2020-12-08 14:08:07 +0500

    last

--- index.php
+++ index.php
@@ -27,155 +27,152 @@ $conn=mysqli_connect($servername,$username,$password,"$dbname");
 <body>
 <!-- Header -->
 <div class="allcontain">
-	<div class="header">
-			<ul class="socialicon">
-				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-				<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-				<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-			</ul>
-			<ul class="givusacall">
-				<li>Give us a call : +66666666 </li>
-			</ul>
-			<ul class="logreg">
-				<li><a href="user_login.php">Login </a> </li>
-				<li><a href="create_user.php"><span class="register">Register</span></a></li>
-			</ul>
-	</div>
-	<!-- Navbar Up -->
-	<nav class="topnavbar navbar-default topnav">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed toggle-costume" data-toggle="collapse" data-target="#upmenu" aria-expanded="false">
-					<span class="sr-only"> Toggle navigaion</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand logo" href="#"><img src="image/logo1.png" alt="logo"></a>
-			</div>	 
-		</div>
-		<div class="collapse navbar-collapse" id="upmenu">
-			<ul class="nav navbar-nav" id="navbarontop">
-				<li class="active"><a href="#">HOME</a> </li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle"	data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">CATEGORIES <span class="caret"></span></a>
-					<ul class="dropdown-menu dropdowncostume">
-						<li><a href="#">Sport</a></li>
-						<li><a href="#">Old</a></li>
-						<li><a href="#">New</a></li>
-					</ul>
-				</li>
-				<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">DEALERS <span class="caret"></span></a>
-						<ul class="dropdown-menu dropdowncostume">
-							<li><a href="#">1</a></li>
-							<li><a href="#">2</a></li>
-							<li><a href="3">3</a></li>
-						</ul>
-				</li>
-				<li>
-					<a href="contact.html">CONTACT</a>
+<div class="header">
+<ul class="socialicon">
+<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+</ul>
+<ul class="givusacall">
+<li>Give us a call : +66666666 </li>
+</ul>
+<ul class="logreg">
+<li><a href="user_login.php">Login </a> </li>
+<li><a href="create_user.php"><span class="register">Register</span></a></li>
+</ul>
+</div>
+<!-- Navbar Up -->
+<nav class="topnavbar navbar-default topnav">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed toggle-costume" data-toggle="collapse" data-target="#upmenu" aria-expanded="false">
+<span class="sr-only"> Toggle navigaion</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand logo" href="#"><img src="image/logo1.png" alt="logo"></a>
+</div>	 
+</div>
+<div class="collapse navbar-collapse" id="upmenu">
+<ul class="nav navbar-nav" id="navbarontop">
+<li class="active"><a href="#">HOME</a> </li>
+<li class="dropdown">
+<a href="#" class="dropdown-toggle"	data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">CATEGORIES <span class="caret"></span></a>
+<ul class="dropdown-menu dropdowncostume">
+<li><a href="#">Sport</a></li>
+<li><a href="#">Old</a></li>
+<li><a href="#">New</a></li>
+</ul>
+</li>
+<li class="dropdown">
+<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">DEALERS <span class="caret"></span></a>
+<ul class="dropdown-menu dropdowncostume">
+<li><a href="#">1</a></li>
+<li><a href="#">2</a></li>
+<li><a href="3">3</a></li>
+</ul>
+</li>
+<li>
+<a href="contact.html">CONTACT</a>
  
-				</li>
-				<button><span class="postnewcar">POST NEW CAR</span></button>
-			</ul>
-		</div>
-	</nav>
+</li>
+<button><span class="postnewcar">POST NEW CAR</span></button>
+</ul>
+</div>
+</nav>
 </div>
 <!--_______________________________________ Carousel__________________________________ -->
 <div class="allcontain">
-	<div id="carousel-up" class="carousel slide" data-ride="carousel">
-		<div class="carousel-inner " role="listbox">
-			<div class="item active">
-				<img src="image/oldcar.jpg" alt="oldcar">
-				<div class="carousel-caption">
-					<h2>Porsche 356</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur ,<br>
-						sed do eiusmod tempor incididunt ut labore </p>
-				</div>
-			</div>
-			<div class="item">
-			<img src='<?= "img/".$row['car_image'] ?>' class="img-fluid" > 
-				<div class="carousel-caption">
-					<h2>Porche</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur ,<br>
-						sed do eiusmod tempor incididunt ut labore </p>
-				</div>
-			</div>
-			<div class="item">
-				<img src="image/benz.jpg" alt="benz">
-				<div class="carousel-caption">
-					<h2>Car</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur ,<br>
-						sed do eiusmod tempor incididunt ut labore </p>
-				</div>
-			</div>
-		</div>
-		<nav class="navbar navbar-default midle-nav">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed textcostume" data-toggle="collapse" data-target="#navbarmidle" aria-expanded="false">
-						<h1>SEARCH TEXT</h1>
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-			</div>
-			<div class="collapse navbar-collapse" id="navbarmidle">
-				<div class="searchtxt">
-					<h1>SEARCH TEXT</h1>
-				</div>
-				<form class="navbar-form navbar-left searchformmargin" role="search">
-					<div class="form-group">
-						<input type="text" class="form-control searchform" placeholder="Enter Keyword">
-					</div>
-				</form>
-				<ul class="nav navbar-nav navbarborder">
-					<li class="li-category">
-						<a class="btn  dropdown-toggle btn-costume"  id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Category<span class="glyphicon glyphicon-chevron-down downicon"></span></a>
-						<ul class="dropdown-menu" id="mydd">
-							<li><a href="#">Epic</a></li>
-							<li><a href="#">Old</a></li>
-							<li><a href="#">New</a></li>
-						</ul>
-					</li>
-					<li class="li-minyear"><a class="btn  dropdown-toggle btn-costume"  id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Min Year <span class="glyphicon glyphicon-chevron-down downicon"></span></a>
-						<ul class="dropdown-menu" id="mydd2">
-							<li><a href="#">1</a></li>
-							<li><a href="#">2</a></li>
-							<li><a href="#">3</a></li>
-						</ul>
-					</li>
-					<li class="li-maxyear"><a class="btn dropdown-toggle btn-costume"  id="dropdownMenu3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Max Year <span class="glyphicon glyphicon-chevron-down downicon"></span></a>
-						<ul class="dropdown-menu" id="mydd3">
-							<li><a href="#">1900</a></li>
-							<li><a href="#">2000</a></li>
-							<li><a href="#">2016</a></li>
-						</ul>
-					</li>
-					<li class="li-slideprice">
-						<p> <label class="slidertxt" for="amount">Price </label>
-							<input class="priceslider" type="text" id="amount" readonly="readonly">
-						</p>
-							<div id="slider-range"></div>
-							
-					</li>
-					<li class="li-search"> <button class="searchbutton"><span class="glyphicon glyphicon-search "></span></button></li>
-				</ul>
- 
-			</div>
-		</nav>
-	</div>
+<div id="carousel-up" class="carousel slide" data-ride="carousel">
+<div class="carousel-inner " role="listbox">
+<div class="item active">
+<img src='<?= "img/".$row['car_image'] ?>' class="img-fluid" > 
+<div class="carousel-caption">
+<h2>Porsche 356</h2>
+<p>Lorem ipsum dolor sit amet, consectetur ,<br> sed do eiusmod tempor incididunt ut labore </p>	
+</div>
+</div>
+<div class="item">
+<img src='<?= "img/".$row['car_image'] ?>' class="img-fluid" > 
+<div class="carousel-caption">
+<h2>Porche</h2>
+<p>Lorem ipsum dolor sit amet, consectetur ,<br>
+sed do eiusmod tempor incididunt ut labore </p>
+</div>
+</div>
+<div class="item">
+<img src="image/benz.jpg" alt="benz">
+<div class="carousel-caption">
+<h2>Car</h2>
+<p>Lorem ipsum dolor sit amet, consectetur ,<br>
+sed do eiusmod tempor incididunt ut labore </p>
+</div>
+</div>
+</div>
+<nav class="navbar navbar-default midle-nav">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed textcostume" data-toggle="collapse" data-target="#navbarmidle" aria-expanded="false">
+<h1>SEARCH TEXT</h1>
+<span class="sr-only">Toggle navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+</div>
+<div class="collapse navbar-collapse" id="navbarmidle">
+<div class="searchtxt">
+<h1>SEARCH TEXT</h1>
+<form class="navbar-form navbar-left searchformmargin" role="search">
+</div>
+<div class="form-group">
+<input type="text" class="form-control searchform" placeholder="Enter Keyword">
+</div>
+</form>
+<ul class="nav navbar-nav navbarborder">
+<li class="li-category">
+<a class="btn  dropdown-toggle btn-costume"  id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Category<span class="glyphicon glyphicon-chevron-down downicon"></span></a>
+<ul class="dropdown-menu" id="mydd">
+<li><a href="#">Epic</a></li>
+<li><a href="#">Old</a></li>
+<li><a href="#">New</a></li>
+</ul>
+</li>
+<li class="li-minyear"><a class="btn  dropdown-toggle btn-costume"  id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Min Year <span class="glyphicon glyphicon-chevron-down downicon"></span></a>
+<ul class="dropdown-menu" id="mydd2">
+<li><a href="#">1</a></li>
+<li><a href="#">2</a></li>
+<li><a href="#">3</a></li>
+</ul>
+</li>
+<li class="li-maxyear"><a class="btn dropdown-toggle btn-costume"  id="dropdownMenu3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Max Year <span class="glyphicon glyphicon-chevron-down downicon"></span></a>
+<ul class="dropdown-menu" id="mydd3">
+<li><a href="#">1900</a></li>
+<li><a href="#">2000</a></li>
+<li><a href="#">2016</a></li>
+</ul>
+</li>
+<li class="li-slideprice">
+<p> <label class="slidertxt" for="amount">Price </label>
+<input class="priceslider" type="text" id="amount" readonly="readonly">
+</p>
+<div id="slider-range"></div>					
+</li>
+<li class="li-search"> <button class="searchbutton"><span class="glyphicon glyphicon-search "></span></button></li>
+</ul>
+ </div>
+</nav>
+</div>
 </div>
 <!-- ____________________Featured Section ______________________________--> 
 <div class="allcontain">
-	<div class="feturedsection">
-		<h1 class="text-center"><span class="bdots">&bullet;</span>F E A T U R E S<span class="carstxt">C A R S</span>&bullet;</h1>
-	</div>
-	<div class="feturedimage">
-		<div class="row firstrow">
-		<?php
+<div class="feturedsection">
+<h1 class="text-center"><span class="bdots">&bullet;</span>F E A T U R E S<span class="carstxt">C A R S</span>&bullet;</h1>
+</div>
+<div class="feturedimage">
+<div class="row firstrow">
+<?php
 $result = mysqli_query($conn,"SELECT * FROM cars_tbl");
 if (mysqli_num_rows($result) > 0) {
 ?> 
@@ -183,42 +180,41 @@ if (mysqli_num_rows($result) > 0) {
 $i=0;
 while($row = mysqli_fetch_array($result)) {
 ?>
-			<div class="col-lg-6 costumcol colborder1">
-				<div class="row costumrow">
-					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 img1colon">
-					<img src='<?= "img/".$row['car_image'] ?>'height="350" width="150" >
-					</div>
-					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 txt1colon ">
-						<div class="featurecontant">
-							<h1><?= $row["car_name"] ; ?></h1>
-							<p><?= $row["car_discription"] ; ?> </p>
-			 				<h2>Price &euro;</h2>
-			 				<button id="btnRM" onclick="rmtxt()">READ MORE</button>
+<div class="col-lg-6 costumcol colborder1">
+<div class="row costumrow">
+<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 img1colon">
+<img src='<?= "img/".$row['car_image'] ?>'height="350" width="150" >
+</div>
+<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 txt1colon ">
+<div class="featurecontant">
+<h1><?= $row["car_name"] ; ?></h1>
+<p><?= $row["car_discription"] ; ?> </p>
+<h2>Price &euro;</h2>
+<button id="btnRM" onclick="rmtxt()">READ MORE</button>
 			 				
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php
+</div>
+</div>
+</div>
+</div>
+</div>
+<?php
 $i++;
 }
 }
 ?>
-
 	</div>
 <!-- ________________________LATEST CARS SECTION _______________________-->
 <div class="latestcars">
-	<h1 class="text-center">&bullet; LATEST   CARS &bullet;</h1>
-	<ul class="nav nav-tabs navbar-left latest-navleft">
-		<li role="presentation" class="li-sortby"><span class="sortby">SORT BY: </span></li>
-		<li data-filter=".RECENT" role="presentation"><a href="#mostrecent" class="prcBtnR">MOST RECENT </a></li>
-		<li data-filter=".POPULAR" role="presentation"><a href="#mostpopular" class="prcBtnR">MOST POPULAR </a></li>
-		<li  role="presentation"><a href="#" class="alphSort">ALPHABETICAL </a></li>
-		<li data-filter=".HPRICE" role="presentation"><a href="#" class="prcBtnH">HIGHEST PRICE </a></li>
-		<li data-filter=".LPRICE" role="presentation"><a href="#" class="prcBtnL">LOWEST  PRICE </a></li>
-		<li id="hideonmobile">
-	</ul>
+<h1 class="text-center">&bullet; LATEST   CARS &bullet;</h1>
+<ul class="nav nav-tabs navbar-left latest-navleft">
+<li role="presentation" class="li-sortby"><span class="sortby">SORT BY: </span></li>
+<li data-filter=".RECENT" role="presentation"><a href="#mostrecent" class="prcBtnR">MOST RECENT </a></li>
+<li data-filter=".POPULAR" role="presentation"><a href="#mostpopular" class="prcBtnR">MOST POPULAR </a></li>
+<li  role="presentation"><a href="#" class="alphSort">ALPHABETICAL </a></li>
+<li data-filter=".HPRICE" role="presentation"><a href="#" class="prcBtnH">HIGHEST PRICE </a></li>
+<li data-filter=".LPRICE" role="presentation"><a href="#" class="prcBtnL">LOWEST  PRICE </a></li>
+<li id="hideonmobile">
+</ul>
 </div>
 <br>
 <br>
